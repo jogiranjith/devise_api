8226cafc5b678cbf953ddf5f38c558a01b46c062
Ranjith <ranjith@cognitiveclouds.com>
2015-11-09 02:05:49 +0530

    Generating devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable and :omniauthable
+ acts_as_token_authenticatable
+ devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   # Autocode: Relationships
 
   # Autocode: Validations
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 	# Engines
   mount ApiTaster::Engine => "/api_taster"
 
