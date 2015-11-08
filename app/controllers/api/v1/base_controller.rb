bc92ccf3e730a8a13dcbc9b7ff85b22c3300a0c1
Ranjith <ranjith@cognitiveclouds.com>
2015-11-09 05:04:55 +0530

    Installed Devise -- Starting with Devise

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,19 +1,19 @@
-require "#{Rails.root}/lib/errors/errors.rb"
+class class Api::V1::BaseController < ApplicationController
+ rescue_from Errors::Error, with: :render_error
+       
+    before_filter :set_default_response_format
 
-class Api::V1::BaseController < ApplicationController
-  rescue_from Errors::Error, with: :render_error
-  #protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
-  before_filter :set_default_response_format
+    
 
-  layout false
+    layout false
 
-  private
+    private
 
-  def render_error(error)
-    render(json: error, status: error.status)
-  end
+    def render_error(error)
+      render(json: error, status: error.status)
+    end
 
-  def set_default_response_format
-    request.format = :json
-  end
+    def set_default_response_format
+      request.format = :json
+    end
 end
---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+ end
